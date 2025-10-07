e8490eedfb8f181d7194f82cda1654907958a99a
Illius77 <dehti1@bfh.ch>
2025-10-07 18:32:25 +0200

    demo1

--- service-worker.js
+++ service-worker.js
@@ -1,45 +1,23 @@
-const CACHE_NAME = "quiz-cache-v1";
-const FILES_TO_CACHE = [
-  "index.html",
-  "style.css",
-  "app.js",
-  "questions.json",
-  "images/accident_sign.png",
-  "images/parked_car.png",
-  "manifest.json"
+const CACHE_NAME = 'auto-quiz-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './app.js',
+  './style.css',
+  './manifest.json',
+  './questions.json',
+  './images/accident_sign.png',
+  './images/parked_car.png'
 ];
 
-self.addEventListener("install", (event) => {
-  console.log("Service Worker installing...");
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(FILES_TO_CACHE);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
-  self.skipWaiting();
 });
 
-self.addEventListener("activate", (event) => {
-  console.log("Service Worker activating...");
-  event.waitUntil(
-    caches.keys().then((keyList) => {
-      return Promise.all(
-        keyList.map((key) => {
-          if (key !== CACHE_NAME) {
-            console.log("Removing old cache:", key);
-            return caches.delete(key);
-          }
-        })
-      );
-    })
-  );
-  self.clients.claim();
-});
-
-self.addEventListener("fetch", (event) => {
+self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
